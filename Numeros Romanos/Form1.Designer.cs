57641733916f74ffaa34f5bdf1b86938057c6aae
julimanesi94 <julimanesi94@gmail.com>
2022-08-29 19:28:14 -0300

    Agregar archivos de proyecto.

---
+++ Numeros Romanos/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Numeros_Romanos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numero = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.palabra_sal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumRomano = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Num_sal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numero
+            // 
+            this.numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero.Location = new System.Drawing.Point(246, 40);
+            this.numero.Margin = new System.Windows.Forms.Padding(2);
+            this.numero.Maximum = new decimal(new int[] {
+            3999,
+            0,
+            0,
+            0});
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(106, 30);
+            this.numero.TabIndex = 0;
+            this.numero.ValueChanged += new System.EventHandler(this.ingreso);
+            this.numero.Enter += new System.EventHandler(this.answer_Enter);
+            this.numero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ingreso);
+            this.numero.Validated += new System.EventHandler(this.ingreso);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "El numero en romano es :";
+            // 
+            // palabra_sal
+            // 
+            this.palabra_sal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.palabra_sal.Location = new System.Drawing.Point(289, 83);
+            this.palabra_sal.Margin = new System.Windows.Forms.Padding(0);
+            this.palabra_sal.Name = "palabra_sal";
+            this.palabra_sal.Size = new System.Drawing.Size(227, 34);
+            this.palabra_sal.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingrese un número";
+            // 
+            // NumRomano
+            // 
+            this.NumRomano.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumRomano.Location = new System.Drawing.Point(316, 119);
+            this.NumRomano.Margin = new System.Windows.Forms.Padding(2);
+            this.NumRomano.Name = "NumRomano";
+            this.NumRomano.Size = new System.Drawing.Size(166, 30);
+            this.NumRomano.TabIndex = 5;
+            this.NumRomano.TextChanged += new System.EventHandler(this.IngresoRomano);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 119);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(246, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ingrese un número romano";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 163);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "El numero en decimal es :";
+            // 
+            // Num_sal
+            // 
+            this.Num_sal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_sal.Location = new System.Drawing.Point(289, 163);
+            this.Num_sal.Margin = new System.Windows.Forms.Padding(0);
+            this.Num_sal.Name = "Num_sal";
+            this.Num_sal.Size = new System.Drawing.Size(227, 38);
+            this.Num_sal.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 200);
+            this.Controls.Add(this.Num_sal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NumRomano);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.palabra_sal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numero);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Conversor decimal---->romano, Romano--->decimal                         Autor: Ju" +
+    "lián Manesi";
+            this.Validated += new System.EventHandler(this.ingreso);
+            ((System.ComponentModel.ISupportInitialize)(this.numero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label palabra_sal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NumRomano;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Num_sal;
+    }
+}
+
---
+++ Numeros Romanos/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Numeros_Romanos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int nro;
+        private void ingreso(object sender, EventArgs e)
+        {
+            nro = (int)numero.Value;
+            palabra_sal.Text = funcion2(nro);
+        }
+
+       
+
+        private string funcion2(int nro)
+        {
+            int milena, nrores;
+            string conc;
+            string[] mil = { "M", "MM", "MMM"};
+            conc = "";
+            milena = nro / 1000;
+            if (milena > 0) {  conc +=mil[milena-1] ; }
+            nrores = nro - (milena * 1000);
+            conc += funcion(nrores);
+            return conc;
+        }
+        /// <summary>
+        /// toma un número como parametro que es menor a 100
+        /// y devuelve el nro en forma coloquial
+        /// </summary>
+        /// <param name="nro"></param>
+        /// <returns></returns>
+        private string funcion(int nro)
+        {
+            int unidad, decena, centena;
+            string[] unidades = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] centenas = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] decenas1 = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string conc;
+            conc = "";
+            centena = nro / 100;
+            decena = (nro - (centena * 100)) / 10;//primero separo el nro en unidades, decenas y centenas
+            unidad = nro - (centena * 100 + decena * 10);
+            if (centena > 0) {conc += centenas[centena-1]; }
+            switch (decena)
+            {
+                case 0: if (unidad == 0) { return conc; }
+                    else
+                    {
+                        conc += unidades[unidad - 1]; return conc;
+                    }
+                    break; //  si no hay decenas muestro unidades solas
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    if (unidad == 0)
+                    {
+                        conc += decenas1[decena - 1];
+                        return conc;
+                    }// si las decenas no tienen unidad  tambien las muestro solas
+                    else
+                    {         // si no concateno 
+                        conc += decenas1[decena - 1];
+                        conc += unidades[unidad - 1];
+                        return conc;
+                    }
+                default: return conc;
+            }
+
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+        /// <summary>
+        /// Ingresa un numero romano en caracteres y devuelve el numero 
+        /// en decimal
+        /// </summary>
+        private void IngresoRomano(object sender, EventArgs e)
+        {
+            string romano = NumRomano.Text;
+            Num_sal.Text = funcRom(romano);
+        }
+        private string funcRom(string romano)
+    {
+        int i = romano.Length - 1, num = 0;
+        string Error = "Error. La expresión ingresada es ilegal";
+        if(IngresoErroneo(romano))
+            return Error;
+        if(romano.Length>0)
+        num += Conv(romano[romano.Length-1]); 
+       
+        for (i = romano.Length-1; i>0; i--)
+        {
+            if (Conv(romano[i]) > Conv(romano[i-1]))
+                num -= Conv(romano[i - 1]);
+            else
+                num += Conv(romano[i - 1]);
+        }
+        
+        return num.ToString();
+        
+    }
+
+
+
+        /// <summary>
+        /// devuelve el numero correspondiente a la letra
+        /// en romano
+        /// </summary>
+        /// <param name="letra"></param>
+        /// <returns></returns>
+        private int Conv(char letra)
+        {
+            
+            switch (letra)
+            {
+                case 'I':
+                case 'i': return 1;
+                case 'V':
+                case 'v': return 5;
+                case 'X':
+                case 'x': return 10;
+                case 'L':
+                case 'l': return 50;
+                case 'C':
+                case 'c': return 100;
+                case 'D':
+                case 'd': return 500;
+                case 'M':
+                case 'm': return 1000;
+                default: return 0;
+            }
+        }
+
+        private bool IngresoErroneo(string romano)
+        {
+            int i;
+            romano.ToUpper();
+            for (i = 0; i < romano.Length-3; i++)
+            {
+                if (romano.Length > 3)
+                {
+                    if (romano[i] == romano[i + 1] && romano[i + 2] == romano[i + 3] && romano[i + 1] == romano[i + 2])
+                        return true;
+                }
+            }
+            for (i = 0; i < romano.Length; i++)
+            {
+                if (romano[i] != 'I' && romano[i] != 'V' && romano[i] != 'X' && romano[i] != 'L' && romano[i] != 'D' && romano[i] != 'C' && romano[i] != 'M' && romano[i] != 'i' && romano[i] != 'v' && romano[i] != 'x' && romano[i] != 'l' && romano[i] != 'c' && romano[i] != 'd' && romano[i] != 'm')
+                    return true;
+            }
+            for (i = 0; i < romano.Length - 1; i++)
+            {
+                
+                if (romano.Length > 1)
+                {
+                    if ((romano[i] == 'V' || romano[i] == 'L' || romano[i] == 'D' || romano[i] == 'v' || romano[i] == 'l' || romano[i] == 'd') && romano[i] == romano[i + 1])
+                        return true;
+                    if ((romano[i] == 'I' || romano[i] == 'i') && (romano[i + 1] != 'I' && romano[i + 1] != 'X' && romano[i + 1] != 'V' && romano[i + 1] != 'x' && romano[i + 1] != 'i' && romano[i + 1] != 'v'))
+                        return true;
+                    if ((romano[i] == 'X' || romano[i] == 'x') && (romano[i + 1] == 'D' || romano[i + 1] == 'd' || romano[i + 1] == 'm' || romano[i + 1] == 'M'))
+                        return true;
+                }
+                
+            }
+            return false;
+        }
+
+        private void ingreso(object sender, KeyPressEventArgs e)
+        {
+            nro = (int)numero.Value;
+            palabra_sal.Text = funcion2(nro);
+        }
+    }
+}
